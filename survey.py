3332a377d5f609fdc57651703c8686c78709a14f
Tristan Goodwill <48371600+tristangdwl@users.noreply.github.com>
2023-10-02 21:39:49 -0500

    Update survey.py

--- survey.py
+++ survey.py
@@ -6,7 +6,6 @@ Print answers to the following
 3. What are your academic interests? (research/coursework)
 4. What programming languages do you have experience with?
 5. What is your experience with Python?  (is is ok to have no experience)
-6. What time zone are you in? (Chicago is UTC -5)
-7. What is something you would like to learn in this course?
-8. Do you have any questions or concerns you would like to share?
+6. What is something you would like to learn in this course?
+7. Do you have any questions or concerns you would like to share?
 """
